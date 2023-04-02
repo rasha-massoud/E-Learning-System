9dbe4046a2db9187f87cc5ce4338f4d485175a7c
RashaM <rashamassoud1998@gmail.com>
2023-04-02 12:12:35 +0300

    Installed the cluster package and fork workers processors to work

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,20 @@
+const cluster = require("cluster");
+const OS = require("os")
 const express = require("express");
 const app = express();
 app.use(express.json())
 require("dotenv").config();
 
-app.listen(process.env.PORT, (err) => {
-    if (err) console.error(err)
-    console.log(`Server is running on port `, process.env.PORT);
-    require("./configs/db.config")
-  });
+if (cluster.isMaster) {
+    const numCpus = OS.cpus().length;
+    for (let i = 0; i < numCpus; i++) {
+      cluster.fork();
+    }
+  } else {
+    app.listen(process.env.PORT, (err) => {
+      if (err) console.error(err)
+      console.log(`Worker ${process.pid} is running on port `, process.env.PORT);
+      require("./configs/db.config")
+    });
+  }
+  
