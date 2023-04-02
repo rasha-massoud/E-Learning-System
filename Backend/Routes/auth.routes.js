6afb85075b2f2d918a7648322cd581d039188174
RashaM <rashamassoud1998@gmail.com>
2023-04-02 17:58:25 +0300

    Created the login and register apis

--- Backend/Routes/auth.routes.js
+++ Backend/Routes/auth.routes.js
@@ -1,7 +1,7 @@
-const { Router} = require("express");
-const router= Router();
+const { Router } = require("express");
+const router = Router();
 
-const { login, register } = require("../Controllers/auth.controllers")
+const { login, register } = require("../Controllers/auth.controllers");
 
 router.get("/login", login);
 router.post("/register", register);
