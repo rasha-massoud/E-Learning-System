7008510db407808a121feda86d9798d703b5f6c8
RashaM <rashamassoud1998@gmail.com>
2023-04-02 16:40:12 +0300

    Added the course model and created the schema

---
+++ Backend/Models/courseModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    start_date: {
+        type: Date,
+        required: true
+    },
+    end_date: {
+        type: Date,
+        required: true
+    },
+    enrolled_students: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    files: [{
+        name: String,
+        type: String,
+        url: String
+    }],
+    withdrawal_requests: [{
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        status: {
+            type: String,
+            enum: ['Pending', 'Approved', 'Rejected'],
+            default: 'Pending'
+        }
+    }]
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
