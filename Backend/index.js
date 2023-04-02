c3dc8b5dc726f713e483018ec906beb608a03819
RashaM <rashamassoud1998@gmail.com>
2023-04-02 12:03:20 +0300

    Started the server and started to listen to requests thorugh app.listen in index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 require("dotenv").config();
+
+app.listen(process.env.PORT, (err) => {
+    if (err) console.error(err)
+    console.log(`Server is running on port `, process.env.PORT);
+    require("./configs/db.config")
+  });
