580224992f7fdcda102247e8340204e3b191e2d4
RashaM <rashamassoud1998@gmail.com>
2023-04-04 01:28:28 +0300

    Modified the download file FE page

--- FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
+++ FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
@@ -1,27 +1,56 @@
-
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./styles.css";
 
 function ViewAndDownloadFiles() {
 
-    const [file, setFiles] = useState([]);
+    const [files, setFiles] = useState([]);
+    const [courseId, setCourseId] = useState('course_id'); // replace with the actual course ID
+    const [fileId, setFileId] = useState(null);
+
     useEffect(() => {
-        axios.get('http://localhost:8000/api/v0.0.1/view_and_download_files', {
+        if (fileId) {
+            axios.get(`http://localhost:3000/course/${courseId}/files/${fileId}/download`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                responseType: 'blob'
+            })
+                .then(response => {
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', fileId);
+                    document.body.appendChild(link);
+                    link.click();
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }, [courseId, fileId]);
+
+    const handleDownload = (fileId) => {
+        setFileId(fileId);
+    };
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/course/download/${courseId}/${fileId}`, {
             headers: {
-                'content-type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
-            }
+            },
+            responseType: 'blob'
         })
             .then(response => {
-                setFiles(response.data.file);
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                setFiles(prevFiles => [...prevFiles, { id: fileId, url }]);
             })
             .catch(error => {
-                error.log(error);
+                console.error(error);
             });
-    }, []);
-
+    }, [courseId, fileId]);
 
     return (
         <div>
@@ -29,12 +58,13 @@ function ViewAndDownloadFiles() {
             <div className="container">
                 {files.map(file => (
                     <div key={file.id} className="box1">
-                        <h2 className="center">{file.name}</h2>
-                        <pre>{file.content}</pre>
+                        <h2 className="center">{file.id}</h2>
+                        <button onClick={() => handleDownload(file.id)}>Download</button>
                     </div>
                 ))}
             </div>
         </div>
     );
 };
+
 export default ViewAndDownloadFiles;
