6615bbcf4452f7d37427a6136d5ae30cf9c86b17
RashaM <rashamassoud1998@gmail.com>
2023-04-08 00:48:29 +0300

    Modified the download file page (frontend)

--- FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
+++ FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
@@ -6,37 +6,9 @@ function ViewAndDownloadFiles() {
 
     const [files, setFiles] = useState([]);
     const [courseId, setCourseId] = useState('course_id'); // replace with the actual course ID
-    const [fileId, setFileId] = useState(null);
 
-    useEffect(() => {
-        if (fileId) {
-            axios.get(`http://localhost:3000/course/${courseId}/files/${fileId}/download`, {
-                headers: {
-                    'Accept': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem('token')
-                },
-                responseType: 'blob'
-            })
-                .then(response => {
-                    const url = window.URL.createObjectURL(new Blob([response.data]));
-                    const link = document.createElement('a');
-                    link.href = url;
-                    link.setAttribute('download', fileId);
-                    document.body.appendChild(link);
-                    link.click();
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
-    }, [courseId, fileId]);
-
-    const handleDownload = (fileId) => {
-        setFileId(fileId);
-    };
-
-    useEffect(() => {
-        axios.get(`http://localhost:3000/course/download/${courseId}/${fileId}`, {
+    const handleDownload = (fileUrl) => {
+        axios.get(`http://localhost:3000/download/${courseId}/files/${fileUrl}`, {
             headers: {
                 'Accept': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
@@ -45,18 +17,42 @@ function ViewAndDownloadFiles() {
         })
             .then(response => {
                 const url = window.URL.createObjectURL(new Blob([response.data]));
-                setFiles(prevFiles => [...prevFiles, { id: fileId, url }]);
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', fileUrl.substring(fileUrl.lastIndexOf('/') + 1));
+                document.body.appendChild(link);
+                link.click();
             })
             .catch(error => {
                 console.error(error);
             });
-    }, [courseId, fileId]);
+    };
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/course/files/${courseId}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(response => {
+                setFiles(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, [courseId]);
 
     return (
-        <div>
+        <div className="download-block">
             <h1 className="title">FILES</h1>
             <div className="container">
-                
+                {files.map(file => (
+                    <div key={file._id} className="file">
+                        <h3>{file.name}</h3>
+                        <button onClick={() => handleDownload(file.url)}>Download</button>
+                    </div>
+                ))}
             </div>
         </div>
     );
