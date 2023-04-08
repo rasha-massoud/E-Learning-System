c57cf3c6498d9d559355adf021a637fbcabd692d
RashaM <rashamassoud1998@gmail.com>
2023-04-08 11:52:55 +0300

    Modified the Upload from the FE

--- FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
+++ FrontendClient/frontend-client/src/Components/ViewAndDownloadFiles/index.jsx
@@ -5,28 +5,49 @@ import "./styles.css";
 function ViewAndDownloadFiles() {
 
     const [files, setFiles] = useState([]);
-    const [courseId, setCourseId] = useState('course_id'); // replace with the actual course ID
+    const [courseId, setCourseId] = useState('');
 
-    const handleDownload = (fileUrl) => {
-        axios.get(`http://localhost:3000/download/${courseId}/files/${fileUrl}`, {
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-            },
-            responseType: 'blob'
-        })
-            .then(response => {
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', fileUrl.substring(fileUrl.lastIndexOf('/') + 1));
-                document.body.appendChild(link);
-                link.click();
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
+    const handleCourseIdChange = (e) => {
+        setCourseId(e.target.value);
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        // axios.post('http://localhost:3000/course/enroll', {
+        //     courseId: courseId
+        // }, {
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'authorisation': 'Bearer ' + localStorage.getItem('token')
+        //     }
+        // })
+        // .then(response => {
+        // })
+        // .catch(error => {
+        //     console.error("Enrollment Failed!");
+        // });
+    }
+
+    // const handleDownload = (fileUrl) => {
+    //     axios.get(`http://localhost:3000/download/${courseId}/files/${fileUrl}`, {
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Authorization': 'Bearer ' + localStorage.getItem('token')
+    //         },
+    //         responseType: 'blob'
+    //     })
+    //         .then(response => {
+    //             const url = window.URL.createObjectURL(new Blob([response.data]));
+    //             const link = document.createElement('a');
+    //             link.href = url;
+    //             link.setAttribute('download', fileUrl.substring(fileUrl.lastIndexOf('/') + 1));
+    //             document.body.appendChild(link);
+    //             link.click();
+    //         })
+    //         .catch(error => {
+    //             console.error(error);
+    //         });
+    // };
 
     useEffect(() => {
         axios.get(`http://localhost:3000/course/files/${courseId}`, {
@@ -45,15 +66,16 @@ function ViewAndDownloadFiles() {
 
     return (
         <div className="download-block">
-            <h1 className="title">FILES</h1>
-            <div className="container">
-                {files.map(file => (
-                    <div key={file._id} className="file">
-                        <h3>{file.name}</h3>
-                        <button onClick={() => handleDownload(file.url)}>Download</button>
-                    </div>
-                ))}
-            </div>
+            <h2>FILES</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                    <label htmlFor="courseId">Course Id:</label>
+                    <input type="text" id="courseId" name="courseId" value={courseId} onChange={handleCourseIdChange} autoComplete="off" />
+                </div>
+                <div className="buttons">
+                    <button type="submit">Get Files</button>   
+                </div>
+            </form>
         </div>
     );
 };
